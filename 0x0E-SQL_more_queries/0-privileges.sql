d5f438012e65244b08c152701233283ff9830da6
habtamu12-svg <agsehagis@gmail.com>
2023-02-15 17:40:41 +0300

    list all privileges of mysql users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+1 -- Lists all privileges of the MySQL Users `user_0d_1`
+2 -- and `user_0d_2` in `localhost`.
+3 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+4 SHOW GRANTS FOR 'user_0d_1'@'localhost';
