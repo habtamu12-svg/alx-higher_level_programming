12f1aaf1656223f0829adf15d5796586eedcfcd1
habtamu12-svg <agsehagis@gmail.com>
2023-02-15 17:48:21 +0300

    create user and grant all privilleges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,4 @@
-1  -- Creates a MySQL Server user `user_0d_1`
-2  -- and sets a temporary password `user_0d_1_pwd`.
-3  DROP USER IF EXISTS 'user_0d_1'@'localhost';
-4  CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-5  GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
-6  FLUSH PRIVILEGES;
+1  -- creates the mysql useruser_0d_1
+2  -- give the user all privileges
+3  CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+4  GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
